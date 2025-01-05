f8087eafae6cdd8114ba7b1f98d6e4340a202079
mohkhalidabdulaziz <mohkhalidabdulaziz@gmail.com>
2025-01-05 23:12:28 +0800

    dockerize kafka

--- src/main/java/com/khalid/log_processor/log/consumer/LogConsumer.java
+++ src/main/java/com/khalid/log_processor/log/consumer/LogConsumer.java
@@ -20,7 +20,7 @@ public class LogConsumer {
     @KafkaListener(topics = "app-logs", groupId = "log-processing-group")
     public void consume(String message) {
         if (message.contains("ERROR")) {
-            log.info("Processing error log: {}", message);
+         //   log.info("Processing error log: {}", message);
             LogEntity logEntity = new LogEntity(message, "ERROR");
             logRepository.save(logEntity);
         }
